75c17a67aba7b835999f9aff20cc643690c3485c
Alex Konkin <alexkonkin@gmail.com>
2019-07-13 11:07:49 +0000

    added testrepo module

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+class testrepo {
+     if $environment == 'production' {
+          message => "this is the productioin environment"
+     } else {
+          notify { 'default-message' :
+               message => "This is not a production environment"
+          }
+     }
+}
