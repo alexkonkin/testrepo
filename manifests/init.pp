14718fd0052c9f8d3377abe5a52af621916468e7
alexkonkin <alexkonkin@gmail.com>
2019-07-13 15:22:38 +0300

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 class testrepo {
      if $environment == 'production' {
-          message => "this is the productioin environment"
+         notify { 'default-message' :
+              message => "this is the productioin environment"
+         }
      } else {
           notify { 'default-message' :
                message => "This is not a production environment "
